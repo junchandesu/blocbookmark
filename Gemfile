99c2c6679ccd819a7fe56840c94181bc32451b06
Junko Werner <aliciajace@gmail.com>
2015-07-25 17:09:34 -0700

    Devise, Authentication, Login css

--- Gemfile
+++ Gemfile
@@ -25,6 +25,11 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # layouts, forms, buttons, icons, Javascript functions, and more.
 gem 'bootstrap-sass'
 
+# authenticate users
+gem 'devise'
+# handling sensitive data with environment variables
+gem 'figaro', '1.0'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,12 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+   # defined default URL options, mail delivery method to be active
+   config.action_mailer.default_url_options = { host: 'localhost:3000' }
+   config.action_mailer.delivery_method = :smtp
+   config.action_mailer.perform_deliveries = true
+
+   # Override Action Mailer's 'silent errors' in development
+   config.action_mailer.raise_delivery_errors = true
 end
