9e6f1dce54b8a4e53c4a1f5a56010cc1293fee1f
Junko Werner <aliciajace@gmail.com>
2015-08-06 18:25:08 -0700

    Incoming email nonuser condition added

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -4,42 +4,16 @@ class IncomingController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
 
   def create
-    # Take a look at these in your server logs
-    # to get a sense of what you're dealing with.
-    # puts "INCOMING PARAMS HERE: #{params}"
-
-    # # You put the message-splitting and business
-    # # magic here. 
-
-
-    #  # Find the user by using params[:sender]
-    #  sender = params[:sender]
-    #  # Find the topic by using params[:subject]
-    #  subject = params[:subject]
-    #  # Assign the url to a variable after retreiving it from params["body-plain"]
-    #  url = params["body-plain"]
-    #  # Check if user is nil, if so, create and save a new user
-    #  @user = User.find_by(params[:sender])
-    #  if @user.nil
-    #     @user = User.new(name: "sender", email: sender, password: "helloworld") 
-    #     @user.save_confirmation!
-    #     @user.save
-    #  end
-    #  # Check if the topic is nil, if so, create and save a new topic
-    #  @topic = Topic.find_by(params[:subject])
-    #  if @topic.nil
-    #   @topic = Topic.create(user: @user, title: suject) 
-    #  # Now that you're sure you have a valid user and topic, build and save a new bookmark
-    #   @bookmark = Bookmark.create(url: url, topic: @topic)
-    #  end
-    # Assuming all went well. 
 
     @user = User.where(email: params['sender']).first
     @topic = Topic.where(title: params[:subject]).first_or_create
 
-    if @user
-        @topic.bookmarks.create(url: params['stripped-text'], user: @user)
+    if !@user
+       @user =  User.new(email: params['sender'], name: params['sender'], password: params['sender'])
+       @user.skip_confirmation!
+       @user.save!
     end
+     @topic.bookmarks.create(url: params['stripped-text'], user: @user)
     head 200
   end
 end
